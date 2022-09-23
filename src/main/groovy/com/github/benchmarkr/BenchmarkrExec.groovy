14d3d8ce32629bb7062998236dec736c5c62f939
Tyler Weiss <tnweiss5@gmail.com>
2022-09-23 11:13:46 -0700

    gradle plugin bugfixes

---
+++ src/main/groovy/com/github/benchmarkr/BenchmarkrExec.groovy
@@ -0,0 +1,58 @@
+package com.github.benchmarkr
+
+import org.gradle.api.file.FileCollection
+import org.gradle.api.tasks.JavaExec
+import org.gradle.api.tasks.SourceSet
+import org.gradle.api.tasks.SourceSetContainer
+import org.gradle.api.tasks.compile.JavaCompile
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+abstract class BenchmarkrExec extends JavaExec {
+    public static final String EXTENSION_NAME = "benchmarkConfig"
+    private static final Logger logger = LoggerFactory.getLogger(BenchmarkrExec.class)
+
+    BenchmarkrExec() {
+        Set<FileCollection> compileUrls = new HashSet<>()
+
+        // add all of the dependencies for the test classes
+        try {
+            def compileTestJavaTask = getProject()
+                    .getTasks()
+                    .withType(JavaCompile)
+                    .getByName("compileTestJava")
+            compileUrls.add(compileTestJavaTask.classpath)
+        } catch (Exception ex) {
+            getLogger().info("Skipping compileTestJava...", ex)
+        }
+
+        // add the test and main sources as dependencies
+        SourceSetContainer sourceSets = (SourceSetContainer)getProject().getProperties().get("sourceSets")
+        compileUrls.add(sourceSets.getByName(SourceSet.TEST_SOURCE_SET_NAME).output.classesDirs)
+        compileUrls.add(sourceSets.getByName(SourceSet.MAIN_SOURCE_SET_NAME).output.classesDirs)
+
+        // set the classpath for the task
+        classpath(compileUrls)
+    }
+
+    @Override
+    void exec() {
+        def configExtension = getProject()
+                .getExtensions()
+                .getByName(EXTENSION_NAME) as BenchmarkrExtension
+
+        if (configExtension.packageName().isBlank()) {
+            def newPackageName = getProject().group.toString()
+            logger.info("Overriding package name with ''")
+            configExtension.packageName(newPackageName)
+        }
+
+        def config = configExtension.config()
+        config.entrySet()
+                .stream()
+                .forEach(e -> logger.info("${e.getKey()}: '${e.getValue()}'"))
+        environment(config)
+
+        super.exec()
+    }
+}
---
+++ src/main/groovy/com/github/benchmarkr/BenchmarkrExtension.groovy
@@ -0,0 +1,89 @@
+package com.github.benchmarkr
+
+import groovy.cli.Option
+
+class BenchmarkrExtension {
+    private String packageName = BenchmarkrCore.PACKAGE_PROPERTY_DEFAULT
+    private String className = BenchmarkrCore.CLASS_PROPERTY_DEFAULT
+    private String methodName = BenchmarkrCore.METHOD_PROPERTY_DEFAULT
+    private int iterations = Integer.parseInt(BenchmarkrCore.ITERATIONS_PROPERTY_DEFAULT)
+    private boolean ignoreFailures = false
+    private String console = BenchmarkrCore.CONSOLE_PROPERTY_DEFAULT
+
+    ///////////////////////////////////////////////////// METHODS /////////////////////////////////////////////////////
+    Map<String, ?> config() {
+        Map<String, ?> data = new HashMap<>()
+
+        data.put(BenchmarkrCore.PACKAGE_PROPERTY, packageName)
+        data.put(BenchmarkrCore.CLASS_PROPERTY, className)
+        data.put(BenchmarkrCore.METHOD_PROPERTY, methodName)
+        data.put(BenchmarkrCore.ITERATIONS_PROPERTY, iterations)
+        data.put(BenchmarkrCore.IGNORE_FAILURES_PROPERTY, ignoreFailures)
+        data.put(BenchmarkrCore.CONSOLE_PROPERTY, console)
+
+        return data
+    }
+
+    //////////////////////////////////////////////// SETTERS / GETTERS ////////////////////////////////////////////////
+    @Option(longName = "package-name", shortName = 'p')
+    void packageName(String packageName) {
+        this.packageName = packageName
+    }
+
+    String packageName() {
+        return this.packageName
+    }
+
+    @Option(longName = "class-name", shortName = 'c')
+    void className(String className) {
+        this.className = className
+    }
+
+    String className() {
+        return this.className
+    }
+
+    @Option(longName = "method-name", shortName = 'm')
+    void methodName(String methodName) {
+        this.methodName = methodName
+    }
+
+    String methodName() {
+        return this.methodName
+    }
+
+    @Option(longName = "iterations", shortName = 'i')
+    void iterations(String iterations) {
+        this.iterations = Integer.parseInt(iterations)
+    }
+
+    void iterations(int iterations) {
+        this.iterations = iterations
+    }
+
+    int iterations() {
+        return this.iterations
+    }
+
+    @Option(longName = "ignore-failures", shortName = 'f')
+    void ignoreFailures(String ignoreFailures) {
+        this.ignoreFailures = Boolean.parseBoolean(ignoreFailures)
+    }
+
+    void ignoreFailures(boolean ignoreFailures) {
+        this.ignoreFailures = ignoreFailures
+    }
+
+    boolean ignoreFailures() {
+        return this.ignoreFailures
+    }
+
+    @Option(longName = "console", shortName = 'o')
+    void console(String console) {
+        this.console = console
+    }
+
+    String console() {
+        return this.console
+    }
+}
